eef07fcaa4030640ea97c2e26f2d8fea31a1e2b1
Syntax Squad <mayurharisangam10@gmail.com>
2025-06-07 08:55:38 +0000

    Modifed setup.py and trials.ipynb

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = "Telecom-User-Churn",
     version= '0.0.1',
-    author="Syntax Squad",
-    author_email="mayurharisangam10@gmail.com"   
+    author="Syntax Squad",    
     packages= find_packages(),
     install_requires = []
 )
